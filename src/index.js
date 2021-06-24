1399656bc3d846427e3fdccd2255f4581cc8423e
Julian Beitinger <49885564+AnimaComedenti@users.noreply.github.com>
2021-06-24 17:06:17 +0200

    Carousel Bilder größen angepasst, bsp Daten eingefügt

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-function Movie(name, imageUrl) {
+function Movie(name,imageUrl,erscheinungsjahr,regissuer,darsteller) {
     const self = this;
     self.name = name;
     self.imageUrl = imageUrl || 'https://www.carlsen.de/sites/default/files/produkt/cover/harry-potter-und-der-halbblutprinz--28harry-potter-6-29.jpg';
+    self.regissuer = regissuer;
+    self.erscheinungsjahr = erscheinungsjahr;
+    self.darsteller = darsteller;
 }
 
 function Category(name, movieList) {
@@ -10,22 +13,42 @@ function Category(name, movieList) {
     self.movies = movieList;
 }
 
-const movies = [
-    new Movie('Harry Potter 1'),
-    new Movie('Harry Potter 2'),
-    new Movie('Harry Potter 3'),
-    new Movie('Harry Potter 4'),
-    new Movie('Harry Potter 5'),
-    new Movie('Harry Potter 6'),
-    new Movie('Harry Potter 7'),
+const newMovies = [
+    new Movie('Luca',"https://lumiere-a.akamaihd.net/v1/images/image_2ff75a5c.jpeg?region=0%2C0%2C540%2C810"),
+    new Movie('A Family',"https://www.whats-on-netflix.com/wp-content/uploads/2021/06/Japanese-Crime-Drama-Movie-A-Family-Everything-We-Know-So-Far-poster.png"),
+    new Movie('Fatherhood'),
+    new Movie('Twist'),
+    new Movie('Life in a Year'),
+    new Movie('Awake'),
+    new Movie('Playhouse'),
+]
+
+const watchAgainMovies = [
+    new Movie('Avengers Endgame'),
+    new Movie('Avengers Infinity War'),
+    new Movie('Deadpool'),
+    new Movie('Deadpool 2'),
+    new Movie('Matrix'),
+    new Movie('Spiderman'),
+    new Movie('Spiderman 2'),
+]
+
+const trendyMovies = [
+    new Movie('Joker',"https://m.media-amazon.com/images/I/514c0f7PX6L.jpg"),
+    new Movie('The Forest',"https://m.media-amazon.com/images/I/514c0f7PX6L.jpg"),
+    new Movie('Forrest Gump',"https://m.media-amazon.com/images/I/514c0f7PX6L.jpg"),
+    new Movie('Iron Man',"https://m.media-amazon.com/images/I/514c0f7PX6L.jpg"),
+    new Movie('Shrek Der tollkühne Held',"https://m.media-amazon.com/images/I/514c0f7PX6L.jpg"),
+    new Movie('The Wolf of Wall Street',"https://m.media-amazon.com/images/I/514c0f7PX6L.jpg"),
+    new Movie('Harry Potter und der Stein der Weisen',"https://m.media-amazon.com/images/I/514c0f7PX6L.jpg"),
 ]
 
 function ViewModel() {
     const self = this;
     self.categories = [
-        new Category('Neue Filme',movies),
-        new Category('Derzeit beliebt',movies),
-        new Category('Nochmal ansehen',movies),
+        new Category('Neue Filme',newMovies),
+        new Category('Derzeit beliebt',trendyMovies),
+        new Category('Nochmal ansehen',watchAgainMovies),
     ]
 }
 
@@ -34,7 +57,7 @@ ko.applyBindings(new ViewModel());
 let items = document.querySelectorAll('.carousel .carousel-item')
 
 items.forEach((el) => {
-    const minPerSlide = 4
+    const minPerSlide = 6
     let next = el.nextElementSibling
     for (var i=1; i<minPerSlide; i++) {
         if (!next) {
